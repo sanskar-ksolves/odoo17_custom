d591de9a830654ddc2f4f2e85d0826e234af0ff4
Sanskar Sahu <sanskar.sahu@ksolves.com>
2024-07-02 13:07:04 +0530

    Re-add odoo directory without nested repository

---
+++ odoo/addons/my_custom_module/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'My Custom Module',
+    'version': '1.0',
+    'summary': 'A simple custom module for demonstration',
+    'sequence': 10,
+    'description': 'This is a simple custom module created for learning purposes.',
+    'category': 'Custom',
+    'author': 'Your Name',
+    'website': 'http://www.yourwebsite.com',
+    'depends': ['base'],
+    'data': [
+        'views/my_model_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
